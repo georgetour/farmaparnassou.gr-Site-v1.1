a0d2f29693f67b770b9746d7ead1c580650c2743
George Tourtsinakis <tourtsinakis@outlook.com>
2016-11-02 21:57:49 +0200

    Half main mobile page ready
    - Top footer added
    - Main logo
    - Background image

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="images/imageedit.gif">
+    <title>Farmaparnassou.gr - Ελληνικά Προϊόντα</title>
+
+    <!--Bootstrap-->
+    <link  href="css/bootstrap.min.css" rel="stylesheet" type="text/css" >
+
+    <!--Custom Stylesheets for each screen-->
+    <link  href="css/smallscreen/smallscreen.css" rel="stylesheet" type="text/css">
+
+    <!--Font Awesome Icons-->
+    <link href="css/font-awesome/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+<div class="container"><!--Main container-->
+<footer ><!--Footer start --->
+    <div class="row">
+     <!--Left column-->
+     <div class="col-xs-5">
+        <ul>
+            <li>
+                <a class="badge social linkedin" href="https://gr.linkedin.com/in/georgetourtsinakis" target="_blank">
+                <i class="fa fa-facebook"></i>
+                </a>
+            </li>
+            <li>
+                <a class="badge social linkedin" href="https://gr.linkedin.com/in/georgetourtsinakis" target="_blank">
+                <i class="fa fa-twitter"></i>
+                </a>
+            </li>
+            <li>
+                <a class="badge social linkedin" href="https://gr.linkedin.com/in/georgetourtsinakis" target="_blank">
+                <i class="fa fa-instagram"></i>
+                </a>
+            </li>
+            <li>
+                <a class="badge social linkedin" href="https://gr.linkedin.com/in/georgetourtsinakis" target="_blank">
+                <i class="fa fa-pinterest"></i>
+            </a>
+            </li>
+        </ul>
+     </div>
+
+     <!--Right column-->
+     <div class="col-xs-7 text-right">
+        <div >farmaparnassou.gr 2013-2016 All rights reserved</div>
+        <div><a href="http://www.besmartbesimple.com/" target="_blank">Created by George Tourtsinakis</a></div>
+     </div>
+    </div>
+    <div class="normal-font">Τηλέφωνο : 2104800090</div>
+</footer><!--End footer-->
+
+<header class="header"><!--Main header -->
+    <div><img class="img-test" src="images/logo.png"></div>
+
+</header><!--End main header-->
+
+</div><!--End container-->
+
+
+<script  src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script  src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous">   </script>
+</body>
+</html>
