9aca32488b7c272777a94aa753c4e462d7350808
George Tourtsinakis <tourtsinakis@outlook.com>
2016-11-03 16:44:02 +0200

    Main page finished for small screens
    - Main page finished for small screens
    - TODO slowly adapt to all sizes
    - TODO add links when all pages are finished

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
 
     <!--Font Awesome Icons-->
     <link href="css/font-awesome/font-awesome.min.css" rel="stylesheet" type="text/css">
-
 </head>
 
 <body>
@@ -23,24 +22,24 @@
     <div class="row">
      <!--Left column-->
      <div class="col-xs-5">
-        <ul>
+        <ul class="social-icons">
             <li>
-                <a class="badge social linkedin" href="https://gr.linkedin.com/in/georgetourtsinakis" target="_blank">
+                <a class="badge social facebook" href="https://gr.linkedin.com/in/georgetourtsinakis" target="_blank">
                 <i class="fa fa-facebook"></i>
                 </a>
             </li>
             <li>
-                <a class="badge social linkedin" href="https://gr.linkedin.com/in/georgetourtsinakis" target="_blank">
+                <a class="badge social twitter" href="https://gr.linkedin.com/in/georgetourtsinakis" target="_blank">
                 <i class="fa fa-twitter"></i>
                 </a>
             </li>
             <li>
-                <a class="badge social linkedin" href="https://gr.linkedin.com/in/georgetourtsinakis" target="_blank">
+                <a class="badge social instagram" href="https://gr.linkedin.com/in/georgetourtsinakis" target="_blank">
                 <i class="fa fa-instagram"></i>
                 </a>
             </li>
             <li>
-                <a class="badge social linkedin" href="https://gr.linkedin.com/in/georgetourtsinakis" target="_blank">
+                <a class="badge social pinterest" href="https://gr.linkedin.com/in/georgetourtsinakis" target="_blank">
                 <i class="fa fa-pinterest"></i>
             </a>
             </li>
@@ -57,12 +56,59 @@
 </footer><!--End footer-->
 
 <header class="header"><!--Main header -->
-    <div><img class="img-test" src="images/logo.png"></div>
-
+    <div><img class="img-test" src="images/logo.png" alt="Φάρμα Παρνασσού λογότυπο"></div>
+    <h1 class="text-center quote"><i>Αγάπη στην αξία της διατροφής με ελληνική ποιότητα!<i/></h1>
 </header><!--End main header-->
 
 </div><!--End container-->
 
+
+<!--========BOTTOM ============-->
+<div class="row">
+<!--Navbar-->
+<div class="col-sm-12 col-xs-6">
+ <nav class="navbar navbar-default navbar-fixed-bottom">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">farmaparnassou.gr</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="#">Η ιστορία μας</a></li>
+                <li><a href="#">Το κρεοπωλείο μας</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Προϊόντα <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li><a href="#">Separated link</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Συνταγές</a></li>
+                <li><a href="#">Τι είπαν για εμάς</a></li>
+                <li><a href="#">Επικοινωνία</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!--End container-fluid -->
+ </nav><!--End navbar-->
+</div>
+
+
+
+</div><!--End row-->
+<!--============End Bottom======================-->
 
 <script  src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script  src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous">   </script>
